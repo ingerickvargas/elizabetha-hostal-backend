0fcece9114693e8c3ed6169eae8493fb1fafbe2b
ingerickvargas <ingerickvargash@gmail.com>
2026-04-04 20:09:23 -0500

    configruacion del serve

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,8 @@ const app = buildApp()
 
 const start = async () => {
   try {
-    await app.listen({ port: Number(process.env.PORT) || 3001 })
+    await app.listen({ port: Number(process.env.PORT) || 3001,
+		host: '0.0.0.0'})
     console.log('🚀 Backend running')
   } catch (err) {
     app.log.error(err)
